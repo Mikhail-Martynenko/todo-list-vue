d2655d52b9db7064d19392abd2a39cd311126ba2
Mikhail-Martynenko <misha.martynenko8@yandex.ru>
2023-05-30 14:56:47 +0300

    added component InputWithAction

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,22 @@
 <template>
-    <div class="todoBox">
-        <h1>Todo List</h1>
-        <div class="inputCustom">
-            <input type="text" placeholder="Add a task" v-model="newTask">
-            <button @click="addTask">Add</button>
-        </div>
-        <ul>
-            <li v-for="(task, index) in tasks" :key="index">
-                <input type="checkbox" v-if="!task.completed" @change="completedTask(index)">
-                <span :class="{ completed: task.completed }">{{ task.text }}</span>
-                <button class="delete-button" @click="deleteTask(index)">❌</button>
-            </li>
-        </ul>
-    </div>
+  <div class="todoBox">
+    <h1>Todo List</h1>
+    <InputWithAction :tasks="tasks"/>
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">
+        <input type="checkbox" v-if="!task.completed" @change="completedTask(index)">
+        <span :class="{ completed: task.completed }">{{ task.text }}</span>
+        <button class="delete-button" @click="deleteTask(index)">❌</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup>
 import {ref} from "vue";
+import InputWithAction from "@/components/InputWithAction.vue";
 
 const tasks = ref([])
-const newTask = ref('');
-
-const addTask = () => {
-    if (newTask.value.trim() === '') return;
-    tasks.value.push({
-        text: newTask.value,
-        completed: false
-    });
-    newTask.value = '';
-}
 
 const deleteTask = (index) => tasks.value.splice(index, 1)
 
@@ -38,47 +26,42 @@ const completedTask = (index) => tasks.value[index].completed = true;
 
 <style scoped>
 .todoBox {
-    display: flex;
-    flex-direction: column;
-    border: 1px solid hsla(160, 100%, 37%, 1);
-    color: hsla(160, 100%, 37%, 1);
-    padding: 25px;
-    gap: 20px;
+  display: flex;
+  flex-direction: column;
+  border: 1px solid hsla(160, 100%, 37%, 1);
+  color: hsla(160, 100%, 37%, 1);
+  padding: 25px;
+  gap: 20px;
 }
 
 h1 {
-    text-align: center;
+  text-align: center;
 }
 
 ul {
-    padding: 0;
-}
-
-.inputCustom input {
-    padding: 10px;
+  padding: 0;
 }
 
 button {
-    padding: 10px;
-    background-color: hsla(160, 100%, 37%, 1)
+  padding: 10px;
+  background-color: hsla(160, 100%, 37%, 1)
 }
 
 li {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 }
 
 .completed {
-    text-decoration: line-through;
-    color: gray;
+  text-decoration: line-through;
+  color: gray;
 }
 
 .delete-button {
-    border: none;
-    background: transparent;
-    cursor: pointer;
+  border: none;
+  background: transparent;
+  cursor: pointer;
 }
-
 </style>
---
+++ src/components/InputWithAction.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="inputCustom">
+    <input type="text" placeholder="Add a task" v-model="inputText">
+    <button @click="addTask">Add</button>
+  </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+const props = defineProps({
+  tasks: {
+    type: Array,
+  },
+});
+const inputText = ref('');
+const addTask = () => {
+  if (inputText.value.trim() === '') return;
+
+  props.tasks.push({
+    text: inputText.value,
+    completed: false
+  });
+  inputText.value = '';
+}
+</script>
+
+<style scoped>
+.inputCustom input {
+  padding: 10px;
+}
+</style>
